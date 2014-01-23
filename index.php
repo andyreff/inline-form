8c12c3fb671cb3e26b2f1bc50d20c46c0b67f541
Andy Reff <andrew@andrewreff.com>
2014-01-23 15:17:40 -0500

    Inline Forms
    Allows users to create a form using a fill-in the blank look for input
    forms.

---
+++ index.php
@@ -0,0 +1,38 @@
+<? include '../includes/header.php'; ?>
+<!-- Add Additional JS/CSS -->
+<style type="text/css" media="screen">@import url(include/styles/screen.css);</style>
+<!-- Do not edit above this line -->
+    <div id="base">
+        <div id="section-content">
+            <div class="interior-structure">
+            	<h2>Inline Forms</h2>
+                <form action="" method="get" accept-charset="utf-8">
+                  <p class="mad-lips">
+                    Hello, my name is
+                    <label>
+                      <input type="text" name="firstname" value="" />
+                      <span>firstname</span>
+                    </label>
+                    <label>
+                      <input type="text" name="lastname" value="" />
+                      <span>lastname</span>
+                    </label>
+                    and I'm writing you today to learn more about the <strong>2003 HONDA ACCORD LX</strong> listed for <strong>$8,455</strong>. I live in the
+                    <label>
+                      <input type="text" name="zip" value="" />
+                      <span>zip</span>
+                    </label>
+                     area and I would like to hear back from you soon and learn more about this vehicle. Please call me at
+                    <label>
+                      <input type="text" name="zip" value="" />
+                      <span>my phone number</span>
+                    </label>
+                    at your earliest convenience.
+                  </p>
+                  <p><button class="cta" id="postcomment" title="go" type="submit">go</button></p>
+                </form>	                
+            </div>         
+        </div>		
+    </div> 
+<!-- Do not edit below this line -->
+<? include '../includes/footer.php'; ?>
